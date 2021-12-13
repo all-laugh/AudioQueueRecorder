94814a4108e1129bfbee412ff393b8d08789ec34
Xiao Quan 全笑 <58939349+all-laugh@users.noreply.github.com>
2021-12-13 12:43:56 -0500

    Add capability to allow audio input. Recorder is now functional. Phew!

--- AudioQueueRecorder/main.m
+++ AudioQueueRecorder/main.m
@@ -5,7 +5,7 @@
 //  Created by Xiao Quan on 12/13/21.
 //
 
-#import <Foundation/Foundation.h>
+//#import <Foundation/Foundation.h>
 #include <AudioToolbox/AudioToolbox.h>
 
 #define kNumberOfRecordBuffers 3
@@ -157,10 +157,11 @@ static void AQInputCallback(void *inUserData,
                                          inPacketDesc,
                                          recorder->recordPacket,
                                          &inNumPackets,
-                                         inBuffer),
+                                         inBuffer->mAudioData),
                    "AudioFileWritePackets failed");
         
         recorder->recordPacket += inNumPackets;
+        
     }
     
     if (recorder->running) {
@@ -174,104 +175,103 @@ static void AQInputCallback(void *inUserData,
 
 // MARK: - Main Function
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // Set up format 4.4-4.7
-        Recorder recorder = {0};
-        AudioStreamBasicDescription recordFormat;
-        memset(&recordFormat, 0, sizeof(recordFormat));
-        
-        recordFormat.mFormatID = kAudioFormatMPEG4AAC;
-        recordFormat.mChannelsPerFrame = 2;
-        
-        GetDefaultInputDeviceSampleRate(&recordFormat.mSampleRate);
-        
-        UInt32 propSize = sizeof(recordFormat);
-        CheckError(AudioFormatGetProperty(kAudioFormatProperty_FormatInfo,
-                                          0,
-                                          NULL,
-                                          &propSize,
-                                          &recordFormat),
-                   "AudioFormatGetProperty failed");
-        
-        // Set up Queue 4.8-4.9
-        AudioQueueRef queue = {0};
-        CheckError(AudioQueueNewInput(&recordFormat,
-                                      AQInputCallback,
-                                      &recorder,
-                                      NULL,
-                                      NULL,
+    // Set up format 4.4-4.7
+    Recorder recorder = {0};
+    AudioStreamBasicDescription recordFormat;
+    memset(&recordFormat, 0, sizeof(recordFormat));
+    
+    recordFormat.mFormatID = kAudioFormatMPEG4AAC;
+    recordFormat.mChannelsPerFrame = 2;
+    
+    GetDefaultInputDeviceSampleRate(&recordFormat.mSampleRate);
+    
+    UInt32 propSize = sizeof(recordFormat);
+    CheckError(AudioFormatGetProperty(kAudioFormatProperty_FormatInfo,
                                       0,
-                                      &queue),
-                   "AudioQueueNewInput failed");
+                                      NULL,
+                                      &propSize,
+                                      &recordFormat),
+               "AudioFormatGetProperty failed");
+    
+    // Set up Queue 4.8-4.9
+    AudioQueueRef queue = {0};
+    CheckError(AudioQueueNewInput(&recordFormat,
+                                  AQInputCallback,
+                                  &recorder,
+                                  NULL,
+                                  NULL,
+                                  0,
+                                  &queue),
+               "AudioQueueNewInput failed");
+    
+    /* Retrieve filled ASBD from queue*/
+    UInt32 size = sizeof(recordFormat);
+    CheckError(AudioQueueGetProperty(queue,
+                                     kAudioConverterCurrentOutputStreamDescription,
+                                     &recordFormat,
+                                     &size),
+               "failed to get asbd format from queue");
+    
+    // Set up file 4.10-4.11
+    CFURLRef fileURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault,
+                                                     CFSTR("./output.caf"),
+                                                     kCFURLPOSIXPathStyle,
+                                                     false);
+    
+    CheckError(AudioFileCreateWithURL(fileURL,
+                                      kAudioFileCAFType,
+                                      &recordFormat,
+                                      kAudioFileFlags_EraseFile,
+                                      &recorder.recordFile),
+               "AudioFileCreateWithURL failed");
+    CFRelease(fileURL);
+    
+    /* Copy magic cookies if applicable */
+    CopyEncoderCookieToFile(queue, recorder.recordFile);
+    
+    // Other set ups 4.12-4.13
+    // Allocate and put buffers into queue
+    int bufferByteSize = ComputeRecordBufferSize(&recordFormat, queue, 0.5);
+    
+    int bufferIndex;
+    for (bufferIndex = 0;
+         bufferIndex < kNumberOfRecordBuffers;
+         ++bufferIndex) {
+        AudioQueueBufferRef buffer;
+        CheckError(AudioQueueAllocateBuffer(queue,
+                                            bufferByteSize,
+                                            &buffer),
+                   "AudioQueueAllocateBuffer failed");
         
-        /* Retrieve filled ASBD from queue*/
-        UInt32 size = sizeof(recordFormat);
-        CheckError(AudioQueueGetProperty(queue,
-                                         kAudioConverterCurrentOutputStreamDescription,
-                                         &recordFormat,
-                                         &size),
-                   "failed to get asbd format from queue");
-        
-        // Set up file 4.10-4.11
-        CFURLRef fileURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault,
-                                                         CFSTR("output.caf"),
-                                                         kCFURLPOSIXPathStyle,
-                                                         false);
-        
-        CheckError(AudioFileCreateWithURL(fileURL,
-                                          kAudioFileCAFType,
-                                          &recordFormat,
-                                          kAudioFileFlags_EraseFile,
-                                          &recorder.recordFile),
-                   "AudioFileCreateWithURL failed");
-        CFRelease(fileURL);
-        
-        /* Copy magic cookies if applicable */
-        CopyEncoderCookieToFile(queue, recorder.recordFile);
-        
-        // Other set ups 4.12-4.13
-        // Allocate and put buffers into queue
-        int bufferByteSize = ComputeRecordBufferSize(&recordFormat, queue, 0.5);
-        
-        int bufferIndex;
-        for (bufferIndex = 0;
-             bufferIndex < kNumberOfRecordBuffers;
-             bufferIndex++) {
-            AudioQueueBufferRef buffer;
-            CheckError(AudioQueueAllocateBuffer(queue,
-                                                bufferByteSize,
-                                                &buffer),
-                       "AudioQueueAllocateBuffer failed");
-            
-            CheckError(AudioQueueEnqueueBuffer(queue,
-                                               buffer,
-                                               0,
-                                               NULL),
-                       "AudioQueueEnqueueBuffer failed");
-        }
-        
-        // Start queue 4.14-4.15
-        recorder.running = TRUE;
-        CheckError(AudioQueueStart(queue,
-                                   NULL),
-                   "AudioQueueStart failed");
-        
-        printf("Recording, press <return> to stop: \n");
-        getchar();
-        
-        // Stop queue 4.16 - 4.18
-        printf("* recording done *\n");
-        recorder.running = FALSE;
-        CheckError(AudioQueueStop(queue,
-                                  TRUE),
-                   "AudioQueueStop failed");
-        
-        CopyEncoderCookieToFile(queue, recorder.recordFile);
-        
-        /* Clean up */
-        AudioQueueDispose(queue,
-                          TRUE);
-        AudioFileClose(recorder.recordFile);
+        CheckError(AudioQueueEnqueueBuffer(queue,
+                                           buffer,
+                                           0,
+                                           NULL),
+                   "AudioQueueEnqueueBuffer failed");
     }
+    
+    // Start queue 4.14-4.15
+    recorder.running = TRUE;
+    CheckError(AudioQueueStart(queue,
+                               NULL),
+               "AudioQueueStart failed");
+    
+    printf("Recording, press <return> to stop: \n");
+    getchar();
+    
+    // Stop queue 4.16 - 4.18
+    printf("* recording done *\n");
+    recorder.running = FALSE;
+    CheckError(AudioQueueStop(queue,
+                              TRUE),
+               "AudioQueueStop failed");
+    
+    CopyEncoderCookieToFile(queue, recorder.recordFile);
+    
+    /* Clean up */
+cleanup:
+    AudioQueueDispose(queue,
+                      TRUE);
+    AudioFileClose(recorder.recordFile);
     return 0;
 }
