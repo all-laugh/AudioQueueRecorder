dcef19992db89506ff2cbd4866ebafb49979b974
Xiao Quan 全笑 <58939349+all-laugh@users.noreply.github.com>
2021-12-13 09:54:38 -0500

    Initial Commit

---
+++ AudioQueueRecorder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AudioQueueRecorder
+//
+//  Created by Xiao Quan on 12/13/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
